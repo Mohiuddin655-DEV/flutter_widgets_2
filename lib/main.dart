b98dc20ff11293f9e71b84e60e5f14d8bb188758
Md. Mohi-Uddin <mohiuddin655.bd@com>
2022-12-01 13:30:35 +0600

    Responsive Naming Text View

--- lib/main.dart
+++ lib/main.dart
@@ -28,25 +28,28 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(20),
-            ),
-            margin: const EdgeInsets.all(20),
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: const [
-                ResponsiveNamingTextView(name: 'Name1', value: 'Value1'),
-                ResponsiveNamingTextView(name: 'Name2', value: 'Value2'),
-                ResponsiveNamingTextView(name: 'Name3', value: 'Value3'),
-                ResponsiveNamingTextView(name: 'Name4', value: 'Value4'),
-                ResponsiveNamingTextView(name: 'Name5', value: 'Value5'),
-              ],
-            ),
+      appBar: AppBar(
+        title: const Text('Responsive Naming Text View'),
+        elevation: 0,
+        backgroundColor: Colors.black.withOpacity(0.25),
+      ),
+      body: Center(
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          margin: const EdgeInsets.all(20),
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              ResponsiveNamingTextView(name: 'Name1', value: 'Value1'),
+              ResponsiveNamingTextView(name: 'Name2', value: 'Value2'),
+              ResponsiveNamingTextView(name: 'Name3', value: 'Value3'),
+              ResponsiveNamingTextView(name: 'Name4', value: 'Value4'),
+              ResponsiveNamingTextView(name: 'Name5', value: 'Value5'),
+            ],
           ),
         ),
       ),
